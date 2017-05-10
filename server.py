10b23b81c285b006c441f3149ef5a753ba21271b
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-05-10 14:10:42 -0700

    Added templates, phone number functionality

--- server.py
+++ server.py
@@ -1,21 +1,53 @@
 from twilio.rest import Client
+
 import os
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, jsonify, render_template, redirect, request, flash,
+                   session)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+import json
+
+app = Flask(__name__)
+
+app.secret_key = "ks3sn4kynsna87d6f5"
+
+app.jinja_env.undefined = StrictUndefined
 
 
 access_token = os.environ['TWILIO_AUTH_TOKEN']
 access_sid = os.environ['TWILIO_ACCOUNT_SID']
 
 
-# Your Account SID from twilio.com/console
 account_sid = access_sid
-# Your Auth Token from twilio.com/console
 auth_token  = access_token
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+12018050073", 
-    from_="+12013659575",
-    body="Hello from Python!")
+@app.route('/', methods=["GET"])
+def index():
+    """Homepage"""
 
-print(message.sid)
+    return render_template("body.html")
+
+@app.route('/', methods=["POST"])
+def index_process():
+    """Execute text message"""
+
+    phone_number = request.form.get("phone")
+
+    message = client.messages.create(
+        to="+1" + phone_number, 
+        from_="+12013659575",
+        body="Hello from Python!")
+
+    print(message.sid)
+
+    return redirect("/")
+
+if __name__ == '__main__':
+
+    app.run()
