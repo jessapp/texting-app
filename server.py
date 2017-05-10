b754ddaca8493e33d0d9953f56c086a972e7733d
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-05-10 15:36:56 -0700

    Added fact randomization

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from twilio.rest import Client
 
+from random import randint
+
 import os
 
 from jinja2 import StrictUndefined
@@ -39,10 +41,20 @@ def index_process():
 
     phone_number = request.form.get("phone")
 
+
+    facts = {1: "The panda's scientific name is Ailuropoda melanolecua, which means 'black and white cat foot.'",
+             2: "Pandas walk with their front paws turned inward.",
+             3: "The Chinese word for panda is 'xiongmao,'' which means 'bear cat.'",
+             4: "Pandas like to eat bamboo because they have no umami taste receptors, so meat tastes bland to them.",
+             5: "Adult pandas have to eat as much as 80 pounds of bamboo every day to meet their nutritional needs.",
+             6: "Pandas have lived on earth for 2 to 3 million years."}
+
+    random_num = randint(1,6)
+
     message = client.messages.create(
         to="+1" + phone_number, 
         from_="+12013659575",
-        body="Hello from Python!")
+        body=facts[random_num])
 
     print(message.sid)
 
